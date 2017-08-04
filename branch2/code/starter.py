93e025b4d214b84a011270b1a13b1f90a5ee9fcf
Mitchell Pebbles <mitchell.jeffrey.pebbles@gmail.com>
2017-08-04 15:45:11 -0700

    working on adding to prereq wrapper

--- branch2/code/starter.py
+++ branch2/code/starter.py
@@ -3,24 +3,50 @@ class PrereqWrapper:
     def __init__(self):
         # check depth, can have multiple depths
         # check if type PrereqWrapper or Course when doing work (nesting)
-        AND = []
-        OR = []
+        self.AND = []
+        self.OR = []
+        
+    def __repr__(self):
+        return '[AND: {}, OR: {}]'.format(self.AND, self.OR)
 
 # VERY powerful because it combines major info with specific, up to date class info for filtering
 class Course:
     def __init__(self, course_name):
         self.course_name = course_name
+        self.variables = {}
+        self.comment = ''
         self.isRequiredByMajor = None # since dict will be initialized with all prereqs as well
         self.user_has_taken = None
         # preqs before course
-        self.prereq_wrapper = None
+        # list of Course and PrereqWrapper objects
+        self.prereqs = []
         self.coreq_list = []
         self.classes = [] # loaded from database
-        # what is this class a prereq for?
-        self.course_is_prereq_for = None
+        # what is this class a prereq for? - list of course objects
+        # user can possibly take these courses after completing this course
+        self.is_prereq_for = []
         # wrapper to say which courses have to be taken(AND/OR)
         # pull/store info from DB?
         # when is it offered?
+
+    def add_var(self, var, val):
+        self.variables[var] = val
+
+    def add_comment(self, comment):
+        self.comment = comment
+
+    def add_prereq(self, course_obj):
+        self.prereqs.append(course_obj)
+
+    def __str__(self):
+        return 'Course name: {}\n\tvars: {}\n\tcomment: {}\n\tprereq_list: {}\n\tpreq_for_list: {}'.format(
+            self.course_name, self.variables, self.comment, self.prereqs, self.is_prereq_for)
+
+    def __repr__(self):
+        return self.course_name
+
+    def add_is_prereq_for(self, course):
+        self.is_prereq_for.append(course)
 
 def get_major_file(institution, major, type, catalog_year, specialization=None):
     # basic checks of input to function -- won't catch everything (should be done elsewhere)
@@ -83,6 +109,7 @@ def check_basic_syntax_and_remove_comments(major_file):
                     ret_string += char
             else:
                 if char == '\n':
+                    ret_string += char
                     ignore_rest_of_line = False
     if open_square_count > 0 or open_par_count > 0:
         print_error(-1)
@@ -92,6 +119,10 @@ def check_basic_syntax_and_remove_comments(major_file):
 
 # stores info from both .maj file and class database
 def load_major_courses_into_data_structure(major_file):
+
+    # put courses in dictionary for loading all info easily and efficiently
+    # will make objects from below
+    course_dict = {}
 
     # will be a tuple, the first string being the comment to the user about the major, the second being the remaining text
     major_strings = check_basic_syntax_and_remove_comments(major_file)
@@ -106,9 +137,17 @@ def load_major_courses_into_data_structure(major_file):
 
     # process courses first
     for course in second_half.split('*')[1:]:
+        # first course is course to open
+        name = course[course.index('[') + 1: course.index(']')]
+        working_course = None
+        if name in course_dict:
+            working_course = course_dict[name]
+        else:
+            working_course = Course(name)
         local_expression = ''
         local_expression_terminated = False
-        for line in course.splitlines():
+        # skipping first line which was course to open
+        for line in course.splitlines()[1:]:
             if '\\' in line.strip()[-1:]:
                 local_expression_terminated = False
                 local_expression += line[0:-1]
@@ -117,10 +156,37 @@ def load_major_courses_into_data_structure(major_file):
                 local_expression += line
             if local_expression_terminated:
                 # process expression here
-                #print(local_expression)
-                # WORKING HERE
+                # is var?
+                if '=' in line:
+                    temp = line.split('=')
+                    working_course.add_var(temp[0].strip(), temp[1].strip())
+                # course comment
+                elif '$' in line:
+                    line = line[line.index("'") + 1:]
+                    working_course.add_comment(line[:line.index("'")].strip())
+                # prereqs
+                elif '[' in line:
+                    # no prereq wrapper needed
+                    if line.count('[') == 1:
+                        name = line[line.index('[') + 1: line.index(']')]
+                        if name not in course_dict:
+                            course_dict[name] = Course(name)
+                        course_dict[name].add_is_prereq_for(working_course)
+                        working_course.add_prereq(course_dict[name])
+                    # wrapper needed
+                    else:
+                        # TODO: WORKING HERE - what is prereq course
+                        pass
+                course_dict[working_course.course_name] = working_course
                 local_expression = ''
+    ###
+    for key, val in course_dict.items():
+        print(val)
+        print()
+    ###
 
+    # add to course if it is required
+    # add to coreqs?
     # process major requirements
     for line in first_half.splitlines():
         if '\\' in line.strip()[-1:]:
@@ -134,10 +200,6 @@ def load_major_courses_into_data_structure(major_file):
             #print(expression)
             # WORKING HERE 
             expression = ''
-
-    # put courses in dictionary for loading all info easily and efficiently
-    # will make objects from below
-    course_dict = {}
 
 
 
