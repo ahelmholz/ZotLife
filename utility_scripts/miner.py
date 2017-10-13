2a194553e195948c4e145e0e4349a5484401a192
Mitchell Pebbles <mitchell.jeffrey.pebbles@gmail.com>
2017-10-13 11:58:38 -0700

    Irvine needs to learn consistency

--- utility_scripts/miner.py
+++ utility_scripts/miner.py
@@ -8,8 +8,57 @@ class ParsedCourse:
         self.course_name = course_name
         self.course_text = course_text
         self.course_url = course_url
-        # TODO parse text into fields here
-        #self.course_units = course_units
+        self.course_prereqs = []
+        self.course_coreqs = []
+        self.course_restrictions = ''
+        self.course_units = None # check for None
+        self.special = [] # to be written to file as comments for manual resolution (1 entry == 1 line)
+        self.parse_course_text()
+
+    def parse_course_text(self):
+        search_base_url = 'http://catalogue.uci.edu/' # TODO move to make accessible
+        #print("\n*****************************************************************************")
+        #print(self.course_url + "\n")
+        #print(self.course_text)
+        temp_text = self.course_text # in case I modify the text
+        if 'Units' in temp_text:
+            # assumes unit is a single digit with a space before 'Units'
+            if temp_text[temp_text.index('Units') - 2].isdigit():
+                self.course_units = temp_text[temp_text.index('Units') - 2]
+                #print(self.course_units)
+            else:
+                print("Units not found in {}".format(course.course_name))
+        else:
+            print("Units not found in {}".format(course.course_name))
+        if 'coreq' in temp_text.lower():
+            coreq_text1 = temp_text[temp_text.index("oreq"):] # c left off because of inconsistent capitalization
+            coreq_text2 = coreq_text1[:coreq_text1.index('</p>')]
+            #print(coreq_text2) # TODO: handle
+        if 'prereq' in temp_text.lower():
+            prereq_text1 = temp_text[temp_text.index("rerequisite"):]
+            prereq_text2 = prereq_text1[:prereq_text1.index('</p>')]
+            #print(prereq_text2)
+            if 'or' in prereq_text2:
+                split_on_or = prereq_text2.split(' or ')# must have spaces
+                for x in split_on_or:
+                    if '/search/?P=' in x:
+                        temp_text2 = x[x.index('/search/?P=') + 11:]
+                        temp_text3 = temp_text2[:temp_text2.index('"')] # temp_text3 is also course name
+                        # TODO: construct search url, make call and add to dict
+                        # url looks something like temp_url = "\\ ".join((search_base_url + above_token + tt3).split())
+                        #print(temp_text3)
+                    else:
+                        # TODO: handle 'better' and 'with a grade of C'
+                        # add all others to special
+                        pass
+
+            else:
+                pass
+                # TODO handle by adding to special
+        if 'restriction' in temp_text.lower():
+            res_text1 = temp_text[temp_text.index("Restriction"):]
+            res_text2 = res_text1[:res_text1.index('</p>')]
+            #print(res_text2) # TODO: handle by adding to special
 
 def main():
     start_time = time.time()
@@ -35,6 +84,8 @@ def main():
                     temp_url = "\\ ".join((search_base_url + end_of_url).split())
 
                     #print(temp_url)
+
+                    # TODO: put in def
                     h = requests.get(temp_url).text
                     find = '<div class="searchresult"><h2>' + course_name
                     if find in h:
@@ -55,8 +106,9 @@ def main():
                         parsed_course_dict[course_name] = ParsedCourse(course_name, course_text, temp_url)
                     else:
                         print("Unable to locate info for {} at url {}".format(course_name, temp_url))
+                    # END PUT IN DEF
+                    # TODO share parsed_course_dict among defs
                     # TODO: loop through all, build trees, report missing info for manual review, get units, put into nice output file
-                    # TODO: actually parse info
 
     #for k, v in parsed_course_dict.items():
         #print(k)
